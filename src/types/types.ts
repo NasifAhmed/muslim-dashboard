5e84d42e94c565ac48297fc085360e97796c9d58
Nasif Ahmed <nasif2ahmed@gmail.com>
2024-03-06 14:54:16 +0600

    setup all api data types

---
+++ src/types/types.ts
@@ -0,0 +1,119 @@
+type GeoCodeApiResponseType = {
+    place_id: number;
+    licence: string;
+    osm_type: string;
+    osm_id: number;
+    lat: string;
+    lon: string;
+    category: string;
+    type: string;
+    place_rank: number;
+    importance: number;
+    addresstype: string;
+    name: string;
+    display_name: string;
+    boundingbox: string[];
+};
+
+// Type for prayer time api
+type PrayerTiming = {
+    Fajr: string;
+    Sunrise: string;
+    Dhuhr: string;
+    Asr: string;
+    Sunset: string;
+    Maghrib: string;
+    Isha: string;
+    Imsak: string;
+    Midnight: string;
+    Firstthird: string;
+    Lastthird: string;
+};
+
+type GregorianDate = {
+    date: string;
+    format: string;
+    day: string;
+    weekday: {
+        en: string;
+    };
+    month: {
+        number: number;
+        en: string;
+    };
+    year: string;
+    designation: {
+        abbreviated: string;
+        expanded: string;
+    };
+};
+
+type HijriDate = {
+    date: string;
+    format: string;
+    day: string;
+    weekday: {
+        en: string;
+        ar: string;
+    };
+    month: {
+        number: number;
+        en: string;
+        ar: string;
+    };
+    year: string;
+    designation: {
+        abbreviated: string;
+        expanded: string;
+    };
+    holidays: any[]; // You might want to define a proper type for holidays
+};
+
+type Meta = {
+    latitude: number;
+    longitude: number;
+    timezone: string;
+    method: {
+        id: number;
+        name: string;
+        params: {
+            Fajr: number;
+            Isha: number;
+        };
+        location: {
+            latitude: number;
+            longitude: number;
+        };
+    };
+    latitudeAdjustmentMethod: string;
+    midnightMode: string;
+    school: string;
+    offset: {
+        Imsak: number;
+        Fajr: number;
+        Sunrise: number;
+        Dhuhr: number;
+        Asr: number;
+        Maghrib: number;
+        Sunset: number;
+        Isha: number;
+        Midnight: number;
+    };
+};
+
+type DailyPrayerTiming = {
+    timings: PrayerTiming;
+    date: {
+        readable: string;
+        timestamp: string;
+        gregorian: GregorianDate;
+        hijri: HijriDate;
+    };
+    meta: Meta;
+};
+
+type PrayerTimeApiResponseType = {
+    code: number;
+    status: string;
+    data: DailyPrayerTiming[];
+};
