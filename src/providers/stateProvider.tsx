ee800fc12bf5dde0d08d0cc5e1a497e7c4f02618
Nasif Ahmed <nasif2ahmed@gmail.com>
2024-03-06 14:54:59 +0600

    setup global state management

---
+++ src/providers/stateProvider.tsx
@@ -0,0 +1,143 @@
+"use client";
+
+import useGeocoderApi from "@/hooks/useGeocoderApi";
+import usePrayerTimeApi from "@/hooks/usePrayerTimeApi";
+import { timingFormatter } from "@/lib/utils";
+import { useRouter } from "next/navigation";
+import {
+    Dispatch,
+    ReactNode,
+    createContext,
+    useEffect,
+    useReducer,
+} from "react";
+
+type stateType = {
+    location: GeoCodeApiResponseType | undefined;
+    prayerTimingToday: DailyPrayerTiming | undefined;
+    prayerTimingsAll: DailyPrayerTiming[] | undefined;
+};
+
+type stateAction =
+    | { type: "set timing"; payload: DailyPrayerTiming }
+    | { type: "set timings"; payload: DailyPrayerTiming[] }
+    | { type: "set location"; payload: GeoCodeApiResponseType }
+    | { type: "reset" };
+
+type contexType = {
+    state: stateType;
+    dispatch: Dispatch<stateAction>;
+};
+const initialState: stateType = {
+    location: undefined,
+    prayerTimingToday: undefined,
+    prayerTimingsAll: undefined,
+};
+
+export const StateContext = createContext<contexType>({
+    state: initialState,
+    dispatch: () => initialState,
+});
+
+const StateProvider = ({ children }: { children?: ReactNode }) => {
+    const { getRawTimings } = usePrayerTimeApi();
+    const router = useRouter();
+
+    function reducer(
+        state: typeof initialState,
+        action: stateAction
+    ): stateType {
+        switch (action.type) {
+            case "set location": {
+                return {
+                    ...state,
+                    location: action.payload,
+                };
+            }
+            case "set timing": {
+                return {
+                    ...state,
+                    prayerTimingToday: action.payload,
+                };
+            }
+            case "set timings": {
+                let today = new Date();
+                return {
+                    ...state,
+                    prayerTimingsAll: action.payload,
+                    // prayerTiming: action.payload.data[today.getDate()].timings,
+                };
+            }
+            case "reset": {
+                return {
+                    ...state,
+                    location: undefined,
+                    prayerTimingToday: undefined,
+                    prayerTimingsAll: undefined,
+                };
+            }
+        }
+    }
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    useEffect(() => {
+        if (window.localStorage.getItem("location")) {
+            const valueFromStorage = window.localStorage.getItem(
+                "location"
+            ) as string;
+            dispatch({
+                type: "set location",
+                payload: JSON.parse(valueFromStorage),
+            });
+            console.log("Loaded from storage");
+        } else {
+            router.push("/setup");
+        }
+    }, []);
+
+    useEffect(() => {
+        if (state.location) {
+            const dateToday = new Date();
+            getRawTimings(
+                parseFloat(state.location.lat),
+                parseFloat(state.location.lon)
+            ).then((res) => {
+                console.table(res);
+                if (res) {
+                    dispatch({ type: "set timings", payload: res });
+                    dispatch({
+                        type: "set timing",
+                        payload: res[dateToday.getDate()],
+                    });
+                    // const timingToday = timingFormatter(
+                    //     res[dateToday.getDate() - 1].timings
+                    // );
+                    // if (timingToday) {
+                    //     dispatch({
+                    //         type: "set timing",
+                    //         payload: timingToday,
+                    //     });
+                    //     console.log(
+                    //         "TODAY : ",
+                    //         dateToday.getDate(),
+                    //         timingToday
+                    //     );
+                    // }
+                }
+            });
+        }
+    }, [state.location]);
+
+    return (
+        <StateContext.Provider
+            value={{
+                state,
+                dispatch,
+            }}
+        >
+            {children}
+        </StateContext.Provider>
+    );
+};
+
+export default StateProvider;
