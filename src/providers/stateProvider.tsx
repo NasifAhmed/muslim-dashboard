3c4f2367ba0a9f515701a749c5de329e57d95999
Nasif Ahmed <nasif2ahmed@gmail.com>
2024-03-09 11:43:53 +0600

    save timing data to browser

--- src/providers/stateProvider.tsx
+++ src/providers/stateProvider.tsx
@@ -61,6 +61,11 @@ const StateProvider = ({ children }: { children?: ReactNode }) => {
             }
             case "set timings": {
                 let today = new Date();
+                const jsonValue = { data: action.payload };
+                window.localStorage.setItem(
+                    "timings",
+                    JSON.stringify(jsonValue)
+                );
                 return {
                     ...state,
                     prayerTimingsAll: action.payload,
@@ -80,35 +85,56 @@ const StateProvider = ({ children }: { children?: ReactNode }) => {
     const [state, dispatch] = useReducer(reducer, initialState);
 
     useEffect(() => {
-        if (state.location) {
-            const dateToday = new Date();
-            getRawTimings(
-                parseFloat(state.location.lat),
-                parseFloat(state.location.lon)
-            ).then((res) => {
-                console.table(res);
-                if (res) {
-                    dispatch({ type: "set timings", payload: res });
-                    dispatch({
-                        type: "set timing",
-                        payload: res[dateToday.getDate() - 1],
-                    });
-                    // const timingToday = timingFormatter(
-                    //     res[dateToday.getDate() - 1].timings
-                    // );
-                    // if (timingToday) {
-                    //     dispatch({
-                    //         type: "set timing",
-                    //         payload: timingToday,
-                    //     });
-                    //     console.log(
-                    //         "TODAY : ",
-                    //         dateToday.getDate(),
-                    //         timingToday
-                    //     );
-                    // }
-                }
+        const dateToday = new Date();
+        if (state.prayerTimingsAll) {
+            const firstFajr = new Date(state.prayerTimingsAll[0].timings.Fajr);
+
+            // Check if saved data is of current month or not
+            if (
+                firstFajr.getMonth() !== dateToday.getMonth() &&
+                state.location
+            ) {
+                getRawTimings(
+                    parseFloat(state.location.lat),
+                    parseFloat(state.location.lon)
+                ).then((res) => {
+                    console.table(res);
+                    if (res) {
+                        dispatch({ type: "set timings", payload: res });
+                    }
+                });
+            }
+            dispatch({
+                type: "set timing",
+                payload: state.prayerTimingsAll[dateToday.getDate() - 1],
             });
+        }
+    }, [state.prayerTimingsAll]);
+
+    useEffect(() => {
+        if (state.location) {
+            if (window.localStorage.getItem("timings")) {
+                const valueFromStorage = window.localStorage.getItem(
+                    "timings"
+                ) as string;
+                dispatch({
+                    type: "set timings",
+                    payload: JSON.parse(valueFromStorage).data,
+                });
+                console.log("Loaded timings from storage");
+
+                console.log(JSON.parse(valueFromStorage).data);
+            } else {
+                getRawTimings(
+                    parseFloat(state.location.lat),
+                    parseFloat(state.location.lon)
+                ).then((res) => {
+                    console.table(res);
+                    if (res) {
+                        dispatch({ type: "set timings", payload: res });
+                    }
+                });
+            }
         }
     }, [state.location]);
 
